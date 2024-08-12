5cc8f8117db82e9a4f8acfd1a9be908d120fad02
boonzy2 <149987326+boonzy2@users.noreply.github.com>
2024-08-12 12:28:46 +0800

    a

--- lib/cart.dart
+++ lib/cart.dart
@@ -3,12 +3,14 @@ import 'package:get/get.dart';
 import 'controllers/cart_controller.dart';
 
 class CartPage extends StatelessWidget {
+  // Instantiate the CartController using GetX for state management
   final CartController controller = Get.put(CartController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        // AppBar title
         title: Text(
           'Cart',
           style: TextStyle(color: Colors.black),
@@ -16,15 +18,19 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.yellow.shade700,
         iconTheme: IconThemeData(color: Colors.black),
         actions: [
+          // IconButton to clear the cart
           IconButton(
             icon: Icon(Icons.delete, color: Colors.black),
             onPressed: () {
+              // Call the clearCart method in the CartController
               controller.clearCart();
             },
           ),
         ],
       ),
+      // The body of the CartPage using Obx to reactively display changes
       body: Obx(() {
+        // Check if the cart is empty and display a message if it is
         if (controller.cartItems.isEmpty) {
           return Center(child: Text('Your cart is empty.'));
         }
@@ -33,22 +39,27 @@ class CartPage extends StatelessWidget {
           color: Colors.yellow.shade200,
           child: Column(
             children: [
+              // Expanded widget to allow the ListView to take up available space
               Expanded(
                 child: ListView.builder(
                   itemCount: controller.cartItems.length,
                   itemBuilder: (context, index) {
+                    // Get the current cart item data as a Map
                     var cartItem = controller.cartItems[index].data()
                         as Map<String, dynamic>;
 
+                    // FutureBuilder to calculate the total price of each item
                     return FutureBuilder<double>(
                       future: controller.calculateItemTotal(cartItem),
                       builder: (context, itemSnapshot) {
+                        // Display a loading indicator while calculating the total
                         if (!itemSnapshot.hasData) {
                           return Center(child: CircularProgressIndicator());
                         }
 
                         double itemTotal = itemSnapshot.data!;
 
+                        // Card widget to display cart item details
                         return Card(
                           margin: EdgeInsets.symmetric(
                               vertical: 8.0, horizontal: 16.0),
@@ -61,8 +72,10 @@ class CartPage extends StatelessWidget {
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
+                                // Row to display the item image and details
                                 Row(
                                   children: [
+                                    // Display the item image with rounded corners
                                     ClipRRect(
                                       borderRadius: BorderRadius.circular(8.0),
                                       child: Image.network(
@@ -73,11 +86,13 @@ class CartPage extends StatelessWidget {
                                       ),
                                     ),
                                     SizedBox(width: 10),
+                                    // Expanded widget to allow text to take available space
                                     Expanded(
                                       child: Column(
                                         crossAxisAlignment:
                                             CrossAxisAlignment.start,
                                         children: [
+                                          // Display the item name
                                           Text(
                                             cartItem['name'],
                                             style: TextStyle(
@@ -87,6 +102,7 @@ class CartPage extends StatelessWidget {
                                             ),
                                           ),
                                           SizedBox(height: 5),
+                                          // Display the item price
                                           Text(
                                             '\$${cartItem['price']}',
                                             style: TextStyle(
@@ -97,10 +113,12 @@ class CartPage extends StatelessWidget {
                                         ],
                                       ),
                                     ),
+                                    // Column to display quantity controls
                                     Column(
                                       children: [
                                         Row(
                                           children: [
+                                            // Button to decrease quantity
                                             IconButton(
                                               icon: Icon(
                                                 Icons.remove_circle_outline,
@@ -113,6 +131,7 @@ class CartPage extends StatelessWidget {
                                                     cartItem['quantity']);
                                               },
                                             ),
+                                            // Display the current quantity
                                             Text(
                                               '${cartItem['quantity']}',
                                               style: TextStyle(
@@ -120,6 +139,7 @@ class CartPage extends StatelessWidget {
                                                 color: Colors.black,
                                               ),
                                             ),
+                                            // Button to increase quantity
                                             IconButton(
                                               icon: Icon(
                                                 Icons.add_circle_outline,
@@ -138,6 +158,7 @@ class CartPage extends StatelessWidget {
                                   ],
                                 ),
                                 SizedBox(height: 10),
+                                // Display the list of add-ons as Chips
                                 Wrap(
                                   spacing: 8.0,
                                   children:
@@ -157,6 +178,7 @@ class CartPage extends StatelessWidget {
                                   }).toList(),
                                 ),
                                 SizedBox(height: 10),
+                                // Display the total price for the item
                                 Text(
                                   'Item Total: \$${itemTotal.toStringAsFixed(2)}',
                                   style: TextStyle(
@@ -174,12 +196,14 @@ class CartPage extends StatelessWidget {
                   },
                 ),
               ),
+              // Checkout button at the bottom of the screen
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: SizedBox(
                   width: double.infinity,
                   child: Obx(() => ElevatedButton(
                         onPressed: controller.checkout,
+                        // Display the total price in the checkout button
                         child: Text(
                           'Go to Checkout (\$${controller.totalPrice.value.toStringAsFixed(2)})',
                           style: TextStyle(color: Colors.black),
--- lib/controllers/cart_controller.dart
+++ lib/controllers/cart_controller.dart
@@ -6,16 +6,23 @@ import '../checkout.dart';
 import '../payment_details.dart';
 
 class CartController extends GetxController {
+  // Observable list to hold the items in the cart
   var cartItems = <DocumentSnapshot>[].obs;
+
+  // Observable to keep track of the total price of items in the cart
   var totalPrice = 0.0.obs;
+
+  // Get the current user from Firebase Authentication
   final currentUser = FirebaseAuth.instance.currentUser;
 
   @override
   void onInit() {
     super.onInit();
+    // Load cart items when the controller is initialized
     loadCartItems();
   }
 
+  // Method to load cart items from Firestore
   void loadCartItems() {
     if (currentUser != null) {
       FirebaseFirestore.instance
@@ -24,12 +31,15 @@ class CartController extends GetxController {
           .collection('items')
           .snapshots()
           .listen((snapshot) {
+        // Update the cartItems list whenever there's a change in Firestore
         cartItems.value = snapshot.docs;
+        // Recalculate the total price whenever the cart items change
         calculateTotalPrice();
       });
     }
   }
 
+  // Method to delete a specific cart item from Firestore
   Future<void> deleteCartItem(String itemId) async {
     if (currentUser != null) {
       await FirebaseFirestore.instance
@@ -41,6 +51,7 @@ class CartController extends GetxController {
     }
   }
 
+  // Method to clear all items from the cart
   void clearCart() {
     if (currentUser != null) {
       FirebaseFirestore.instance
@@ -49,6 +60,7 @@ class CartController extends GetxController {
           .collection('items')
           .get()
           .then((snapshot) {
+        // Delete each document in the cart
         for (DocumentSnapshot ds in snapshot.docs) {
           ds.reference.delete();
         }
@@ -56,6 +68,7 @@ class CartController extends GetxController {
     }
   }
 
+  // Method to increment the quantity of a specific item
   void incrementQuantity(String itemId) {
     FirebaseFirestore.instance
         .collection('carts')
@@ -65,6 +78,8 @@ class CartController extends GetxController {
         .update({'quantity': FieldValue.increment(1)});
   }
 
+  // Method to decrement the quantity of a specific item
+  // If quantity reaches 1, the item is removed from the cart
   void decrementQuantity(String itemId, int currentQuantity) {
     if (currentQuantity > 1) {
       FirebaseFirestore.instance
@@ -78,6 +93,7 @@ class CartController extends GetxController {
     }
   }
 
+  // Method to calculate the total price of all items in the cart
   void calculateTotalPrice() async {
     double total = 0.0;
     for (var cartItem in cartItems) {
@@ -88,15 +104,18 @@ class CartController extends GetxController {
     totalPrice.value = total;
   }
 
+  // Method to calculate the total price of a specific item, including add-ons
   Future<double> calculateItemTotal(Map<String, dynamic> cartItem) async {
     double itemPrice = cartItem['price'] ?? 0.0;
     int quantity = cartItem['quantity'] ?? 1;
 
     double addOnsTotal = 0.0;
+    // Loop through the add-ons and calculate their total price
     for (var addOn in cartItem['addOns']) {
       if (addOn is Map<String, dynamic>) {
         addOnsTotal += (addOn['price'] ?? 0.0) * quantity;
       } else if (addOn is String) {
+        // If add-on is a string, fetch its details from Firestore
         var addOnSnapshot = await FirebaseFirestore.instance
             .collection('items')
             .doc(cartItem['itemId'])
@@ -110,9 +129,11 @@ class CartController extends GetxController {
       }
     }
 
+    // Return the total price for this item, including add-ons
     return (itemPrice * quantity) + addOnsTotal;
   }
 
+  // Method to check if the current user has any saved cards
   Future<bool> _checkUserHasSavedCard() async {
     if (currentUser == null) {
       return false;
@@ -127,6 +148,7 @@ class CartController extends GetxController {
     return cardCollection.docs.isNotEmpty;
   }
 
+  // Method to check if the user has enough balance on their saved card
   Future<bool> _checkCardValue(double totalPrice) async {
     if (currentUser == null) {
       return false;
@@ -161,26 +183,32 @@ class CartController extends GetxController {
     return false; // Card value is not sufficient
   }
 
+  // Method to handle the checkout process
   Future<void> checkout() async {
     if (await _checkUserHasSavedCard()) {
       bool canProceedToCheckout = await _checkCardValue(totalPrice.value);
 
       if (canProceedToCheckout) {
+        // Clear the cart and navigate to the checkout page
         clearCart();
         Get.to(() => CheckoutPage());
       } else {
+        // Show a message if the card doesn't have enough value
         Fluttertoast.showToast(
           msg:
               "Card does not have enough value. Please add a card with more value.",
           gravity: ToastGravity.TOP,
         );
+        // Navigate to the payment details page
         Get.to(() => PaymentDetailsPage());
       }
     } else {
+      // Show a message if no payment card is found
       Fluttertoast.showToast(
         msg: "Please add a payment card before proceeding.",
         gravity: ToastGravity.TOP,
       );
+      // Navigate to the payment details page
       Get.to(() => PaymentDetailsPage());
     }
   }
